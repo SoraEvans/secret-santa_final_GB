0b3c2e17f7350c7b9b9ebb07498361d81445cfff
Busulka <83404095+Busulka@users.noreply.github.com>
2023-01-12 22:40:44 +0300

    Состояние "Жеребьёвка проведена"

--- src/components/BoxUsers/BoxUsers.js
+++ src/components/BoxUsers/BoxUsers.js
@@ -1,24 +1,33 @@
 import React, { useState } from 'react';
 import DB from '../../constants/db'
-import { UsersList, UserItem, BoxUsersWrapper } from './style';
+import { BoxUsersWrapper, UserItem, UsersList } from './style';
 import DrawButton from "../draw-button/DrawButton";
 import Modal from "../modal/modal";
 
 const BoxUsers = () => {
   const [showModal, setShowModal] = useState(false);
+  const [drawDone, setDrawDone] = useState(false);
   const userItem = DB.users.map((user) => <UserItem key={user.id}>{user.name}</UserItem>)
-  const openModal = () => {
+  const draw = () => {
+    setDrawDone(true);
     setShowModal(prev => !prev);
-  };
+  }
+
 
   return (
     <BoxUsersWrapper>
       <h2>Участники</h2>
       <UsersList>
         {userItem}
-        <UserItem style={{background: 'rgba(163,163,163,0.47)'}}>+ Добавить участника</UserItem>
+        {drawDone
+          ? null
+          : <UserItem style={{ background: 'rgba(163,163,163,0.47)' }}>+ Добавить участника</UserItem>
+        }
       </UsersList>
-      <DrawButton onClick={openModal} />
+      {drawDone
+        ? null
+        : <DrawButton onClick={draw} />
+      }
       <Modal showModal={showModal} setShowModal={setShowModal} />
     </BoxUsersWrapper>
   )
