c123e06fe73c48489fb30596cc59903274ebc706
Andrey Esman <andreyesman.md@gmil.com>
2023-01-14 18:47:09 +0300

    При отсутствии участников отрисовывается кнопка Добавить участников

--- src/components/BoxUsers/BoxUsers.js
+++ src/components/BoxUsers/BoxUsers.js
@@ -1,36 +1,40 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 import DB from '../../constants/db'
-import { BoxUsersWrapper, UserItem, UsersList } from './style';
-import DrawButton from "../draw-button/DrawButton";
-import Modal from "../modal/modal";
+import { BoxUsersWrapper, UserItem, UsersList } from './style'
+import DrawButton from '../draw-button/DrawButton'
+import Modal from '../modal/modal'
+import NoBoxUsers from "../no-box-users/NoBoxUsers";
 
 const BoxUsers = () => {
-  const [showModal, setShowModal] = useState(false);
-  const [drawDone, setDrawDone] = useState(false);
-  const userItem = DB.users.map((user) => <UserItem key={user.id}>{user.name}</UserItem>)
+  const [showModal, setShowModal] = useState(false)
+  const [drawDone, setDrawDone] = useState(false)
+  const userItem = DB.users.map(user => (
+    <UserItem key={user.id}>{user.name}</UserItem>
+  ))
   const draw = () => {
-    setDrawDone(true);
-    setShowModal(prev => !prev);
+    setDrawDone(true)
+    setShowModal(prev => !prev)
   }
-
 
   return (
     <BoxUsersWrapper>
       <h2>Участники</h2>
-      <UsersList>
-        {userItem}
-        {drawDone
-          ? null
-          : <UserItem style={{ background: 'rgba(163,163,163,0.47)' }}>+ Добавить участника</UserItem>
-        }
-      </UsersList>
-      {drawDone
-        ? null
-        : <DrawButton onClick={draw} />
-      }
+      {DB.users.length ? (
+        <UsersList>
+          {userItem}
+          {drawDone ? null : (
+            <UserItem style={{ background: 'rgba(163,163,163,0.47)' }}>
+              + Добавить участника
+            </UserItem>
+          )}
+        </UsersList>
+      ) : (
+        <NoBoxUsers/>
+      )}
+      {drawDone ? null : <DrawButton onClick={draw} />}
       <Modal showModal={showModal} setShowModal={setShowModal} />
     </BoxUsersWrapper>
   )
 }
 
-export default BoxUsers;
+export default BoxUsers
