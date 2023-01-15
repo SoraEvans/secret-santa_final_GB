350c7dab4bdd50f4fb1b247781e2db9ee26a21d9
Busulka <83404095+Busulka@users.noreply.github.com>
2023-01-15 18:22:42 +0300

    Карточка подопечного

--- src/pages/my-box/components/WardCard/WardCard.js
+++ src/pages/my-box/components/WardCard/WardCard.js
@@ -1,18 +1,50 @@
 import React from 'react'
+import { Link } from "@mui/material";
 import { CarouselButton } from '../../../home/components/carousel/style'
 import { ButtonBlock, TextBlock } from '../MyCard/style'
 
-const WardCard = () => (
-  <TextBlock>
-    <ButtonBlock>
-      Вам еще не назначен Подопечный(
-    </ButtonBlock>
-    <ButtonBlock>
-      Дождитесь проведения жеребьевки, чтобы узнать,
-      кому вы будете дарить подарок
-    </ButtonBlock>
-    <CarouselButton>Создать карточку</CarouselButton>
-  </TextBlock>
-)
+const WardCard = () => {
+
+  const myWard = null;
+  const myCard = null;
+
+
+  if (myCard) {
+    return (
+      <TextBlock>
+        {myWard
+          ?
+          <div>{myWard}</div>
+          :
+          <div>
+            <ButtonBlock>
+              Вам еще не назначен Подопечный(
+            </ButtonBlock>
+            <ButtonBlock>
+              Дождитесь проведения жеребьевки, чтобы узнать,
+              кому вы будете дарить подарок
+            </ButtonBlock>
+          </div>
+        }
+
+      </TextBlock>
+    )
+  }
+  return (
+    <TextBlock>
+      <ButtonBlock>
+        Вам еще не назначен Подопечный(
+      </ButtonBlock>
+      <ButtonBlock>
+        Вам нужно создать свою карточку участника,
+        чтобы принимать участие в игре
+      </ButtonBlock>
+      <Link to="/box" style={{ textDecoration: 'none' }}>
+        <CarouselButton>Создать карточку</CarouselButton>
+      </Link>
+    </TextBlock>
+  )
+
+}
 
 export default WardCard
