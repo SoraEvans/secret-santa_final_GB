913252d73d9a40a0ad575ec83eadefaf7e8d215a
Andrey Esman <andreyesman.md@gmil.com>
2023-01-15 21:12:07 +0300

    Поправил анимацию в уведомлениях. Сделал без опасити

--- src/components/UserNotification/style.js
+++ src/components/UserNotification/style.js
@@ -3,22 +3,25 @@ import styled from 'styled-components'
 export const NotificationBox = styled.div`
   position: absolute;
   top: 90px;
-  right: 166px;
+  right: 200px;
   background-color: #f2eeee;
-  min-height: ${({ active }) => (active ? '100px' : '0')};
+  min-height:100px;
   width: 100%;
   max-width: 400px;
-  opacity: ${({ active }) => (active ? 1 : 0)};
+  visibility: ${({ active }) => (active ? "visible" : "hidden")};
+  transform: translateY(${({ active }) => (active ? "0" : "-15%")});
   padding: 20px;
   text-align: center;
   border-radius: 6px;
-  transition: all 0.3s ease-in-out;
+  transition: all 0.2s ease-in-out;
 
   span {
     font-size: 14px;
     line-height: 17px;
     color: #979797;
   }
+
+
 `
 
-export const NotifiactioList = styled.div``
+export const NotificationList = styled.div``
