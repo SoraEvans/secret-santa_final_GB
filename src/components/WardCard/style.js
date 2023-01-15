657bfd5bb5fa88f7ef80db830920512c6b2083bd
Busulka <83404095+Busulka@users.noreply.github.com>
2023-01-15 17:19:49 +0300

    Моя карточка

---
+++ src/components/WardCard/style.js
@@ -0,0 +1,53 @@
+import styled from "styled-components";
+
+export const CreateCardWrapper = styled.div`
+margin-top: 20px;
+`
+
+export const FormLabel = styled.div`
+width: 299px;
+font-size: 12px;
+line-height: 15px;
+color: #868686;
+margin-top: 10px;
+`
+
+export const InputSection = styled.div`
+display: flex;
+flex-direction: column;
+`
+
+export const Input = styled.input`
+  width: 299px;
+  height: 44px;
+  background-color: #F6F3F3;
+  border: 2px solid #CDCCCC;
+  font-size: 15px;
+  color: #979797;
+  padding: 10px;
+  margin: 10px 0;
+`
+
+export const AvatarList = styled.div`
+display: flex;
+justify-content: space-between;
+width: 299px;
+margin: 10px 0 40px 0;
+`
+
+export const AvatarImg = styled.div`
+width: 59px;
+height: 59px;
+background: #FFBFBF;
+border-radius: 3px;
+`
+
+export const AddAvatarButton = styled.button`
+width: 59px;
+height: 59px;
+background: #ffd5d5;
+font-size: 28px;
+border-radius: 3px;
+border: none;
+cursor: pointer;
+`
