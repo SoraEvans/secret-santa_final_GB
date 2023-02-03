c4b2680af49ea51fd862e18be338ae7342d4dfd9
Man-Green-Tea <iamnambavan@gmail.com>
2023-02-03 16:42:24 +0300

    Подготовлены запросы к БД

--- src/pages/profile/Profile.js
+++ src/pages/profile/Profile.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Divider } from '@mui/material'
 import { useNavigate } from 'react-router-dom'
 import Container from '../../components/style_cont'
@@ -12,15 +12,59 @@ import {
   ProfileSwitch,
   ProfileTop,
   SwitchSection,
-  SwitchText, RemoveDesc
+  SwitchText,
+  RemoveDesc
 } from './styles'
 import { CustomInput } from '../../components/Inputs/Inputs'
 
 function Profile() {
+  const [state, setState] = useState({
+    name: null,
+    email: null,
+    notifications: false,
+    password: null,
+    confirm_password: null
+  })
+
+  const id = localStorage.getItem('userId')
   const navigate = useNavigate()
   const logOut = () => {
     localStorage.clear()
     navigate('/')
+  }
+  console.log(state)
+  const onSubmitSave = async state => {
+    await fetch(`https://backsecsanta.alwaysdata.net/api/user/update/${id}`, {
+      method: 'PATCH',
+      header: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: JSON.stringify({
+        name: state.name,
+        email: state.email,
+        email_notify: state.notifications,
+        newPassword: state.password
+      })
+    })
+      .then(response => response.json())
+      .then(response => {
+        if (response.status === 'success') {
+          // navigate('/box-created')
+        }
+      })
+  }
+
+  const onSubmitDelete = async () => {
+    await fetch(`https://backsecsanta.alwaysdata.net/api/user/delete/${id}`, {
+      method: 'DELETE',
+      header: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      }
+    }).then(response => {
+      if (response.status === 200) {
+        logOut()
+      }
+    })
   }
 
   return (
@@ -35,31 +79,83 @@ function Profile() {
             <Divider />
             <InputSectionTitle>Личные данные</InputSectionTitle>
             <InputSection>
-              <CustomInput id="first" type="text" label="Ваше имя или никнейм" margin="0 0 24px" />
-              <CustomInput id="second" type="text" label="Ваш e-mail" margin="0 0 24px" />
+              <CustomInput
+                id="first"
+                type="text"
+                label="Ваше имя или никнейм"
+                margin="0 0 24px"
+                data-name="name"
+                // onChange={handleChangeState}
+                onChange={event =>
+                  setState({
+                    ...state,
+                    name: event.target.value
+                  })
+                }
+              />
+              <CustomInput
+                id="second"
+                type="email"
+                label="Ваш e-mail"
+                margin="0 0 24px"
+                onChange={event =>
+                  setState({
+                    ...state,
+                    email: event.target.value
+                  })
+                }
+              />
             </InputSection>
             <Divider />
             <InputSectionTitle>Уведомления</InputSectionTitle>
             <SwitchSection>
               <SwitchText>
                 <h4>E-mail</h4>
-                <p>При включенной опции уведомления будут приходить
-                  на email адрес, указанный выше</p>
+                <p>
+                  При включенной опции уведомления будут приходить на email
+                  адрес, указанный выше
+                </p>
               </SwitchText>
-              <ProfileSwitch />
+              <ProfileSwitch
+                checked={state.notifications}
+                name="notifications"
+                onChange={event =>
+                  setState({
+                    ...state,
+                    notifications: event.target.checked
+                  })
+                }
+              />
             </SwitchSection>
             <Divider />
             <InputSectionTitle>Смена пароля</InputSectionTitle>
             <InputSection>
-              <CustomInput id="pass" type="text" label="Новый пароль" margin="0 0 24px" />
-              <CustomInput id="confirm" type="text" label="Подтвердите пароль" margin="0 0 24px" />
+              <CustomInput
+                id="pass"
+                type="password"
+                label="Новый пароль"
+                margin="0 0 24px"
+              />
+              <CustomInput
+                id="confirm"
+                type="password"
+                label="Подтвердите пароль"
+                margin="0 0 24px"
+              />
             </InputSection>
-            <ProfileButton type="submit">Сохранить изменения</ProfileButton>
+            <ProfileButton type="button" onClick={() => onSubmitSave(state)}>
+              Сохранить изменения
+            </ProfileButton>
             <Divider />
             <InputSectionTitle>Удаление профиля</InputSectionTitle>
-            <RemoveDesc>ВАЖНО: После удаления профиля, ваши карточки во всех коробках будут удалены</RemoveDesc>
+            <RemoveDesc>
+              ВАЖНО: После удаления профиля, ваши карточки во всех коробках
+              будут удалены
+            </RemoveDesc>
           </ProfileForm>
-          <ProfileButton type="submit">Удалить профиль</ProfileButton>
+          <ProfileButton type="submit" onClick={() => onSubmitDelete(state)}>
+            Удалить профиль
+          </ProfileButton>
         </div>
       </Container>
     </PageBackground>
