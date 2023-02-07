15134f8642f28948360863165ee511cd9ad97f03
Man-Green-Tea <iamnambavan@gmail.com>
2023-02-07 19:40:00 +0300

    Модалка обратной связи

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,17 +1,74 @@
-import React from 'react'
-import { CopyrightText, FooterEl, FooterText, Wrapper } from './style'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import {
+  ModalCandyTop,
+  ModalCandyBot,
+  CopyrightText,
+  FooterEl,
+  FooterText,
+  Wrapper,
+  ModalInput,
+  ModalForm,
+  ModalSend,
+  ModalHead,
+  ModalCredits
+} from './style'
 import Container from '../style_cont'
+import Modal from '../modal/modal'
+import candy from '../../assets/images/candy.svg'
+import btnBranch from '../../assets/images/regBranch.svg'
 
-const Footer = () => (
-  <FooterEl>
-    <Container>
-      <Wrapper>
-        <FooterText>Помочь проекту</FooterText>
-        <FooterText>Обратная связь</FooterText>
-        <CopyrightText>©Copyright 2022</CopyrightText>
-      </Wrapper>
-    </Container>
-  </FooterEl>
-)
+const Footer = () => {
+  const [showModal, setShowModal] = useState(false)
+  const openModal = () => {
+    setShowModal(prev => !prev)
+  }
 
-export default Footer;
+  return (
+    <FooterEl>
+      <Container>
+        <Wrapper>
+          <FooterText>Помочь проекту</FooterText>
+          <FooterText onClick={openModal}>Обратная связь</FooterText>
+          <CopyrightText>©Copyright 2022</CopyrightText>
+          <Modal
+            padding="85px 110px 54px 110px"
+            showModal={showModal}
+            setShowModal={setShowModal}
+          >
+            <div style={{ position: 'relative' }}>
+              <ModalCandyTop src={candy} alt="" />
+              <ModalHead>
+                <h1>Напишите нам</h1>
+                <p>
+                  Есть вопросы, пожелания, предложения?
+                  <br />
+                  Расскажите нам, заполнив форму ниже
+                </p>
+              </ModalHead>
+              <ModalForm>
+                <ModalInput label="Имя" />
+                <ModalInput label="E-mail" />
+                <ModalInput multiline rows={5} label="Ваше сообщение" />
+              </ModalForm>
+              <div style={{ position: 'relative' }}>
+                <img className="reg-branch" src={btnBranch} alt="" />
+                <ModalSend type="button">Отправить</ModalSend>
+              </div>
+              <ModalCredits>
+                Нажимая &ldquo;Отправить&rdquo;, вы даете согласие на&nbsp;
+                <Link to="/www">
+                  обработку
+                  <br /> персональных данных.
+                </Link>
+              </ModalCredits>
+              <ModalCandyBot src={candy} alt="" />
+            </div>
+          </Modal>
+        </Wrapper>
+      </Container>
+    </FooterEl>
+  )
+}
+
+export default Footer
--- src/components/footer/style.js
+++ src/components/footer/style.js
@@ -1,17 +1,26 @@
 import styled from 'styled-components'
+import { styled as st } from '@mui/material'
+import { Credits, StyledRegBtn } from '../../pages/auth/auth-styles'
+import { AuthInput } from '../Inputs/Inputs'
 
 export const FooterEl = styled.footer`
   background: #212121;
   font-family: Raleway;
   flex-shrink: 0;
-`;
+
+  .reg-branch {
+    position: absolute;
+    right: 88px;
+    bottom: -6px;
+  }
+`
 
 export const Wrapper = styled.div`
   display: flex;
   justify-content: space-evenly;
   align-items: center;
   height: 50px;
-`;
+`
 
 export const FooterText = styled.p`
   text-align: center;
@@ -19,8 +28,8 @@ export const FooterText = styled.p`
   font-size: 20px;
   line-height: 29px;
   cursor: pointer;
-  color: #FFFFFF;
-`;
+  color: #ffffff;
+`
 
 export const CopyrightText = styled.p`
   text-align: center;
@@ -28,4 +37,56 @@ export const CopyrightText = styled.p`
   font-weight: 400;
   font-size: 20px;
   line-height: 29px;
+`
+export const ModalHead = styled.div`
+  margin-bottom: 40px;
+  h1 {
+    font-family: 'Raleway';
+    font-style: normal;
+    font-weight: 600;
+    font-size: 32px;
+    line-height: 38px;
+    text-align: center;
+    color: #000000;
+  }
+
+  p {
+    font-family: 'Raleway';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 18px;
+    line-height: 21px;
+    text-align: center;
+    color: #979797;
+  }
+`
+
+export const ModalCandyTop = styled.img`
+  width: 42px;
+  position: absolute;
+  top: -22px;
+  right: -50px;
+`
+export const ModalCandyBot = styled.img`
+  width: 55px;
+  position: absolute;
+  bottom: 85px;
+  left: -50px;
+  transform: scale(-1, 1);
+`
+export const ModalSend = styled(StyledRegBtn)``
+
+export const ModalInput = st(AuthInput)`
+  width: 485px;
+  height: 55px;
+`
+export const ModalForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 110px;
+`
+export const ModalCredits = styled(Credits)`
+  text-align: center;
 `
