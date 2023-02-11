a936bc0963fc7368996fc7f97e7459a0beeda0a8
Andrey Esman <andreyesman.md@gmil.com>
2023-02-11 13:48:14 +0300

    Связал фронт и бэк уведомлений

--- .prettierrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  singleQuote: true,
-  tabWidth: 2,
-  semi: false,
-  trailingComma: 'none',
-  arrowParens: 'avoid',
-  printWidth: 80
-}
--- src/components/UserNotification/style.js
+++ src/components/UserNotification/style.js
@@ -1,28 +1,28 @@
-import styled from 'styled-components'
-import { StyledIcon } from "../../pages/my-boxes/components/PublicBox/style";
+import styled from 'styled-components';
+import {StyledIcon} from '../../pages/my-boxes/components/PublicBox/style';
 
 export const NotificationBox = styled.div`
-  background-color: #F2E7E7BF;
-  min-height: 100px;
-  width: 100%;
-  max-width: 491px;
-  padding: 25px;
-  border-radius: 0 0 4px 4px;
+	background-color: #f2e7e7bf;
+	min-height: 100px;
+	width: 100%;
+	min-width: 481px;
+	padding: 25px;
+	border-radius: 0 0 4px 4px;
 
-  span {
-    font-size: 16px;
-    line-height: 18.7px;
-    color: #665959;
+	span {
+		font-size: 16px;
+		line-height: 18.7px;
+		color: #665959;
 
-    div {
-      font-weight: bold;
-    }
-  }
-`
+		div {
+			font-weight: bold;
+		}
+	}
+`;
 
 export const NotificationIcon = styled(StyledIcon)`
-  width: 100%;
-  max-width: 77px;
-  height: 77px;
-  margin-right: 19px;
-`
+	width: 100%;
+	max-width: 77px;
+	height: 77px;
+	margin-right: 19px;
+`;
