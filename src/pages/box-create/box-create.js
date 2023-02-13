b8268384fc4454f028950ce5c33a2f699223479e
Man-Green-Tea <iamnambavan@gmail.com>
2023-02-13 20:46:04 +0300

    Фикс роутинга в созданную коробку

--- src/pages/box-create/box-create.js
+++ src/pages/box-create/box-create.js
@@ -1,7 +1,7 @@
-import React, { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
-import Cover from '../../components/CoverCarousel/Cover'
-import BOX_CREATE_IMG from '../../constants/box-create-img'
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Cover from "../../components/CoverCarousel/Cover";
+import BOX_CREATE_IMG from "../../constants/box-create-img";
 import {
   Head,
   Title,
@@ -23,13 +23,14 @@ import {
   CostDiv,
   CostInput,
   AntSwitch,
-  SubTitleBoxCreated, BackImage
-} from './style'
+  SubTitleBoxCreated,
+  BackImage,
+} from "./style";
 
 const BoxCreate = () => {
   const [state, setState] = useState({
-    title: '',
-    cover: '',
+    title: "",
+    cover: "",
     isAnonym: false,
     email: false,
     isPublic: false,
@@ -37,15 +38,15 @@ const BoxCreate = () => {
     draw_starts_at: null,
     isLimit: false,
     cost: null,
-    currency: 'RUB'
-  })
-  const navigate = useNavigate()
+    currency: "RUB",
+  });
+  const navigate = useNavigate();
 
-  const onSubmit = async state => {
-    await fetch('https://backsecsanta.alwaysdata.net/api/box/create', {
-      method: 'POST',
+  const onSubmit = async (state) => {
+    await fetch("https://backsecsanta.alwaysdata.net/api/box/create", {
+      method: "POST",
       header: {
-        'Content-Type': 'application/x-www-form-urlencoded'
+        "Content-Type": "application/x-www-form-urlencoded",
       },
       body: JSON.stringify({
         title: state.title,
@@ -57,36 +58,36 @@ const BoxCreate = () => {
         draw_starts_at: state.draw_starts_at,
         isLimit: state.isLimit,
         cost: state.cost,
-        creator_id: localStorage.getItem('userId'),
-        currency: state.currency
-      })
+        creator_id: localStorage.getItem("userId"),
+        currency: state.currency,
+      }),
     })
       // .then(response => response.text()) // Проверка ответа
       // .then(response => {
       //   console.log(response)
       // })
-      .then(response => response.json())
-      .then(response => {
-        if (response.status === 'success') {
-          localStorage.setItem('isBoxCreated', true)
-          navigate('/box-created')
+      .then((response) => response.json())
+      .then((response) => {
+        if (response.status === "success") {
+          localStorage.setItem("isBoxCreated", true);
+          navigate("/box-created", { state: response.box.id });
         }
-      })
-  }
+      });
+  };
 
-  const handleChangeForm = event => {
-    const field = event.target.getAttribute('data-name')
+  const handleChangeForm = (event) => {
+    const field = event.target.getAttribute("data-name");
     setState({
       ...state,
-      [field]: event.target.value
-    })
-  }
-  const handleChangeSwitch = event => {
+      [field]: event.target.value,
+    });
+  };
+  const handleChangeSwitch = (event) => {
     setState({
       ...state,
-      [event.target.name]: event.target.checked
-    })
-  }
+      [event.target.name]: event.target.checked,
+    });
+  };
   return (
     <BackImage>
       <Container>
@@ -96,56 +97,43 @@ const BoxCreate = () => {
         </Head>
         <div>
           <DivInput>
-            <Input
-              required
-              data-name="title"
-              id="title"
-              type="text"
-              onChange={handleChangeForm}
-            />
+            <Input required data-name="title" id="title" type="text" onChange={handleChangeForm} />
             <Label for="title">Название коробки</Label>
           </DivInput>
           <CoverDiv>
-            <P style={{ marginBottom: '11px' }}>Обложка коробки</P>
+            <P style={{ marginBottom: "11px" }}>Обложка коробки</P>
             <Cover fu={setState} state={state} img={BOX_CREATE_IMG} />
           </CoverDiv>
           <Div>
             <div>
               <P>Анонимность участников</P>
               <SubTitleBoxCreated>
-                При включенной опции участники будут видеть имена других игроков.
-                Когда опция выключена, участники будут видеть только аватарки
-                игроков и имя своего подопечного. Организатор будет видеть имена
-                игроков вне зависимости от состояния опции.
+                При включенной опции участники будут видеть имена других игроков. Когда опция выключена, участники будут
+                видеть только аватарки игроков и имя своего подопечного. Организатор будет видеть имена игроков вне
+                зависимости от состояния опции.
               </SubTitleBoxCreated>
             </div>
             <AntSwitch
               checked={state.isAnonym}
               name="isAnonym"
               onChange={handleChangeSwitch}
-              inputProps={{ 'aria-label': 'controlled' }}
+              inputProps={{ "aria-label": "controlled" }}
             />
           </Div>
-          <Div
-            style={
-              state.isPublic ? { marginBottom: '38px' } : { marginBottom: '38px' }
-            }
-          >
+          <Div style={state.isPublic ? { marginBottom: "38px" } : { marginBottom: "38px" }}>
             <div>
               <P>Сделать коробку публичной</P>
               <SubTitleBoxCreated>
-                При включенной опции к вашей коробке смогут присоединиться
-                случайные пользователи. Необходимо будет указать максимальное
-                число участников для коробки, а также выбрать дату автомвтического
-                проведения жеребьевки. Жеребьевка будет проведена независимо от
-                достижения максимума участников.
+                При включенной опции к вашей коробке смогут присоединиться случайные пользователи. Необходимо будет
+                указать максимальное число участников для коробки, а также выбрать дату автомвтического проведения
+                жеребьевки. Жеребьевка будет проведена независимо от достижения максимума участников.
               </SubTitleBoxCreated>
             </div>
             <AntSwitch
               checked={state.isPublic}
               name="isPublic"
               onChange={handleChangeSwitch}
-              inputProps={{ 'aria-label': 'controlled' }}
+              inputProps={{ "aria-label": "controlled" }}
             />
           </Div>
           {state.isPublic ? (
@@ -159,11 +147,9 @@ const BoxCreate = () => {
                   min="0"
                   onChange={handleChangeForm}
                 />
-                <SmallLabel for="max_people_in_box">
-                  Введите максимальное количество участников
-                </SmallLabel>
+                <SmallLabel for="max_people_in_box">Введите максимальное количество участников</SmallLabel>
               </DivInput>
-              <DivInput style={{ marginBottom: '0' }}>
+              <DivInput style={{ marginBottom: "0" }}>
                 <SmallInput
                   required
                   data-name="draw_starts_at"
@@ -172,44 +158,29 @@ const BoxCreate = () => {
                   min="2022-12-01"
                   onChange={handleChangeForm}
                 />
-                <SmallLabel for="draw_starts_at">
-                  Выберите дату проведения автоматической жеребьевки
-                </SmallLabel>
+                <SmallLabel for="draw_starts_at">Выберите дату проведения автоматической жеребьевки</SmallLabel>
               </DivInput>
             </div>
           ) : null}
           <div>
-            <Div
-              style={
-                state.isLimit
-                  ? { marginBottom: '20px' }
-                  : { marginBottom: '38px' }
-              }
-            >
+            <Div style={state.isLimit ? { marginBottom: "20px" } : { marginBottom: "38px" }}>
               <div>
                 <P>Ограничить стоимость подарков</P>
                 <SubTitleBoxCreated>
-                  При включенной опции участникам будет показано ограничение,
-                  которому они должны будут следовать. Ограничение будет показано
-                  на странице подопечного
+                  При включенной опции участникам будет показано ограничение, которому они должны будут следовать.
+                  Ограничение будет показано на странице подопечного
                 </SubTitleBoxCreated>
               </div>
               <AntSwitch
                 checked={state.isLimit}
                 name="isLimit"
                 onChange={handleChangeSwitch}
-                inputProps={{ 'aria-label': 'controlled' }}
+                inputProps={{ "aria-label": "controlled" }}
               />
             </Div>
             {state.isLimit ? (
               <CostDiv>
-                <CostInput
-                  required
-                  data-name="cost"
-                  type="number"
-                  min="0"
-                  onChange={handleChangeForm}
-                />
+                <CostInput required data-name="cost" type="number" min="0" onChange={handleChangeForm} />
                 <Select data-name="currency" onChange={handleChangeForm}>
                   <option value="RUB">RUB</option>
                   <option value="EUR">EUR</option>
@@ -218,7 +189,7 @@ const BoxCreate = () => {
             ) : null}
           </div>
           <ButtonsDiv>
-            <CancellButton type="button" onClick={() => navigate('/boxes')}>
+            <CancellButton type="button" onClick={() => navigate("/boxes")}>
               Отмена
             </CancellButton>
             <CreateButton type="button" onClick={() => onSubmit(state)}>
@@ -228,7 +199,7 @@ const BoxCreate = () => {
         </div>
       </Container>
     </BackImage>
-  )
-}
+  );
+};
 
-export default BoxCreate
+export default BoxCreate;
--- src/pages/box-create/box-done.js
+++ src/pages/box-create/box-done.js
@@ -1,40 +1,36 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import {
-  Button,
-  ContainerBoxCreated,
-  SubTitle,
-  TitleBoxCreated,
-  Img, BoxCreated
-} from './style'
-import BoxImg from '../../assets/images/opening-box.svg'
-
+import React from "react";
+import { useNavigate, useLocation } from "react-router-dom";
+import { Button, ContainerBoxCreated, SubTitle, TitleBoxCreated, Img, BoxCreated } from "./style";
+import BoxImg from "../../assets/images/opening-box.svg";
 
 function BoxDone() {
+  const { state } = useLocation();
+  const navigate = useNavigate();
+
   return (
-
-      <ContainerBoxCreated>
-        <BoxCreated>
-          <TitleBoxCreated>Коробка создана!</TitleBoxCreated>
-          <SubTitle style={{
-            marginBottom: '125px',
-            fontSize: '24px',
-            lineHeight: '34px',
-            color: '#A19593'
-          }}>
-            Теперь можно заглянуть внутрь и добавить новых участников, чтобы провести жеребьевку и распределить Сант.
-          </SubTitle>
-          {/* todo сделать навигацию в созданную коробку */}
-          <Link to="/boxes" style={{ textDecoration: 'none' }}>
-            <Button type="button">Заглянуть в коробку</Button>
-          </Link>
-        </BoxCreated>
-        <Img>
-          <img alt="box" src={BoxImg}/>
-        </Img>
-      </ContainerBoxCreated>
-
-  )
+    <ContainerBoxCreated>
+      <BoxCreated>
+        <TitleBoxCreated>Коробка создана!</TitleBoxCreated>
+        <SubTitle
+          style={{
+            marginBottom: "125px",
+            fontSize: "24px",
+            lineHeight: "34px",
+            color: "#A19593",
+          }}
+        >
+          Теперь можно заглянуть внутрь и добавить новых участников, чтобы провести жеребьевку и распределить Сант.
+        </SubTitle>
+        {/* todo сделать навигацию в созданную коробку */}
+        <Button type="button" onClick={() => navigate(`/box/${state}`)}>
+          Заглянуть в коробку
+        </Button>
+      </BoxCreated>
+      <Img>
+        <img alt="box" src={BoxImg} />
+      </Img>
+    </ContainerBoxCreated>
+  );
 }
 
-export default BoxDone
+export default BoxDone;
